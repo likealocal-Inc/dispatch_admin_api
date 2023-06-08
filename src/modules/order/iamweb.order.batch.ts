19aa9e0d0f9df48d16ae6030e4ab7a351a71defd
seungjinhan <hanblues@gmail.com>
2023-06-08 15:48:48 +0900

    code 정리

--- src/modules/order/iamweb.order.batch.ts
+++ src/modules/order/iamweb.order.batch.ts
@@ -6,6 +6,8 @@ import { PrismaService } from 'src/config/core/prisma/prisma.service';
 import { DefaultConfig } from 'src/config/default.config';
 import { NewIamwebOrderModel } from 'src/libs/core/models/iamweb.order.model';
 import { IamwebUtils } from 'src/libs/core/utils/iamweb.utils';
+import { CustomException } from '../../config/core/exceptions/custom.exception';
+import { ExceptionCodeList } from 'src/config/core/exceptions/exception.code';
 
 @Injectable()
 export class IamwebOrderBatch {
@@ -22,44 +24,51 @@ export class IamwebOrderBatch {
   async orderBatch() {
     const info = '아임웹 주문데이터 조회';
     console.log(`----------- ${info} -------------`);
-    const res: NewIamwebOrderModel[] =
-      await this.iamwebUtils.getOrderListFromIamweb(this.prisma);
+    try {
+      const res: NewIamwebOrderModel[] =
+        await this.iamwebUtils.getOrderListFromIamweb(this.prisma);
 
-    if (res === undefined) return;
+      if (res === undefined) return;
 
-    for (let index = 0; index < res.length; index++) {
-      const d = res[index];
-      await this.prisma.orders.create({
-        data: {
-          status: OrderStatus.IAMWEB_ORDER,
-          isIamweb: true,
-          iamwebOrderNo: d.order_no,
+      for (let index = 0; index < res.length; index++) {
+        const d = res[index];
+        await this.prisma.orders.create({
+          data: {
+            status: OrderStatus.IAMWEB_ORDER,
+            isIamweb: true,
+            iamwebOrderNo: d.order_no,
 
-          orderTime: '' + d.order_time,
-          orderTitle: d.order_title,
+            orderTime: '' + d.order_time,
+            orderTitle: d.order_title,
 
-          boardingDate: d.boarding_date + ' ' + d.boarding_time,
+            boardingDate: d.boarding_date + ' ' + d.boarding_time,
 
-          startLocation: d.start_name,
-          startAddress: d.start_address,
-          startAirport: d.start_airport,
+            startLocation: d.start_name,
+            startAddress: d.start_address,
+            startAirport: d.start_airport,
 
-          goalLocation: d.goal_name,
-          goalAddress: d.goal_address,
-          goalAirport: d.goal_airport,
+            goalLocation: d.goal_name,
+            goalAddress: d.goal_address,
+            goalAirport: d.goal_airport,
 
-          information: JSON.stringify(d.info),
+            information: JSON.stringify(d.info),
 
-          company: 'likealocal',
-          else01:
-            d.start_goal === ''
-              ? ''
-              : `{"start_goal":"${d.start_goal}" ,"trip_route":"${d.trip_route}", "timezon":"${d.timezon}"}`,
-          else02: '',
+            company: 'likealocal',
+            else01:
+              d.start_goal === ''
+                ? ''
+                : `{"start_goal":"${d.start_goal}" ,"trip_route":"${d.trip_route}", "timezon":"${d.timezon}"}`,
+            else02: '',
 
-          userId: DefaultConfig.iamwebApi.iamwebOrderUserId,
-        },
-      });
+            userId: DefaultConfig.iamwebApi.iamwebOrderUserId,
+          },
+        });
+      }
+    } catch {
+      throw new CustomException(
+        ExceptionCodeList.ERROR,
+        '아임웹스케쥴 작동 오류',
+      );
     }
   }
 }
