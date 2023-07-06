8ccb9fe49542a1ecb512c1958930a84e67d2c98a
seungjinhan <hanblues@gmail.com>
2023-07-06 13:21:43 +0800

    cross domain 해결

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
         ? ['error', 'warn', 'log']
         : ['error', 'warn', 'log', 'verbose', 'debug'],
   });
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('/api');
   setSwagger(app);
